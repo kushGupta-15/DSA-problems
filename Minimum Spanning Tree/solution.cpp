5e4c78847eee3d2bd5ab280b126bf66be259e200
Kush Gupta <kushg1517@gmail.com>
2026-08-23 17:06:42 +0530

    Add Minimum Spanning Tree

--- Minimum Spanning Tree/solution.cpp
+++ Minimum Spanning Tree/solution.cpp
@@ -1,52 +1,67 @@
+class DisjointSet {
+    vector<int> size, parent;
+public:
+    DisjointSet(int n) {
+        size.resize(n+1);
+        parent.resize(n+1, 0);
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int findUParent(int node) {
+        if(parent[node] == node) {
+            return node;
+        }
+        
+        return findUParent(parent[node]);
+    }
+    
+    void unionBySize(int u, int v) {
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+        
+        if(ulp_u == ulp_v)
+            return;
+            
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
 class Solution {
   public:
-    int spanningTree(int V, vector<vector<int>>& edges) {
-        // adj[node] stores a list of {neighbor, weight} pairs
-        vector<vector<pair<int,int>>> adj(V);
-
-        // FIX: structured bindings don't work on vector<int> (dynamic size),
-        // so extract u, v, wt by index instead
-        for(auto &edge : edges) {
+    int spanningTree(int V, vector<vector<int>>& edges) { 
+        vector<pair<int, pair<int, int>>> st;
+        for(vector<int>& edge : edges) {
             int u = edge[0];
             int v = edge[1];
             int wt = edge[2];
-
-            adj[u].push_back({v, wt}); // edge u -> v with weight wt
-            adj[v].push_back({u, wt}); // undirected: also add v -> u
+            
+            st.push_back({wt, {u, v}});
         }
-
-        // min-heap of {weight, node}, ordered by smallest weight first
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> visited(V, false);
-
-        pq.push({0, 0}); // start MST construction from node 0, with 0 initial cost
-
-        int sum = 0;
-        while(!pq.empty()) {
-            auto it = pq.top();
-            pq.pop();
-
+        
+        sort(st.begin(), st.end());
+        
+        int mstWt = 0;
+        DisjointSet ds(V);
+        for(auto& it : st) {
             int wt = it.first;
-            int node = it.second;
-
-            // skip stale entries already locked into the MST
-            if(visited[node] == 1)
-                continue;
-
-            visited[node] = 1;  // lock this node into the MST
-            sum += wt;           // add the cost of the edge that brought us here
-
-            // explore neighbors, push only unvisited ones into the heap
-            for(auto &neighbor : adj[node]) {
-                int adjNode = neighbor.first;
-                int edgeWt = neighbor.second;
-
-                if(!visited[adjNode]) {
-                    pq.push({edgeWt, adjNode});
-                }
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findUParent(u) != ds.findUParent(v)) {
+                mstWt += wt;
+                ds.unionBySize(u, v);
             }
         }
-
-        return sum;
+        
+        return mstWt;
     }
 };
