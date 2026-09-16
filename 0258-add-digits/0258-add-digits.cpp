c3608e0414801ed2cefd523846e9f580723927e2
Kush Gupta <kushg1517@gmail.com>
2026-09-16 14:02:56 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.67%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    int sum = 0;
-
-    int countOfDigits(int num) {
-        int count = 0;
-        sum = 0;
-
-        while(num != 0) {
-            sum += num % 10;
-            num /= 10;
-
-            count++;
-        }
-
-        return count;
-    }
     int addDigits(int num) {
-        while(countOfDigits(num) > 1) {
-            num = sum;
-        }
+        if (num == 0)
+            return 0;
 
-        return sum;
+        if (num % 9 == 0)
+            return 9;
+        return num % 9;
     }
 };
